2eb716049fb78b060c0e76c3adf9895dff24626a
Nikol <143750294+nar-ran@users.noreply.github.com>
2025-06-22 21:01:00 -0500

    Variables de color

--- src/components/Posts/FavoritesPostsComponent.vue
+++ src/components/Posts/FavoritesPostsComponent.vue
@@ -58,56 +58,59 @@
 </script>
 
 <style scoped>
-.container {
-  padding: 40px 20px;
-  max-width: 900px;
-  margin: 0 auto;
-}
+  .container {
+    padding: 40px 20px;
+    max-width: 900px;
+    margin: 0 auto;
+  }
 
-.title-section {
-  font-size: 2em;
-  text-align: center;
-  font-family: var(--font-primary);
-  color: var(--text-color);
-  margin-bottom: 30px;
-}
+  .title-section {
+    font-size: 2em;
+    text-align: center;
+    font-family: var(--font-primary);
+    color: var(--text-color);
+    margin-bottom: 30px;
+  }
 
-.post-card {
-  background: linear-gradient(to right, #c61d6b, #30052e);
-  border-radius: 16px;
-  padding: 22px 26px;
-  margin-bottom: 28px;
-  color: #fff;
-  font-family: var(--font-primary);
-  box-shadow: 0 6px 12px rgba(0, 0, 0, 0.3);
-  transition: transform 0.2s ease;
-}
+  .post-card {
+    background: linear-gradient(
+      to right,
+      var(--primary-color),
+      var(--secondary-color)
+    );
+    border-radius: 16px;
+    padding: 22px 26px;
+    margin-bottom: 28px;
+    color: var(--text-color);
+    font-family: var(--font-primary);
+    box-shadow: 0 6px 12px rgba(0, 0, 0, 0.3);
+    transition: transform 0.2s ease;
+  }
 
-.post-card:hover {
-  transform: scale(1.01);
-}
+  .post-card:hover {
+    transform: scale(1.01);
+  }
 
-.post-header {
-  display: flex;
-  justify-content: space-between;
-  font-size: 1.1em;
-  margin-bottom: 10px;
-  font-weight: bold;
-  color: #fff;
-}
+  .post-header {
+    display: flex;
+    justify-content: space-between;
+    font-size: 1.1em;
+    margin-bottom: 10px;
+    font-weight: bold;
+    color: var(--text-color);
+  }
 
-.post-date {
-  font-size: 0.95em;
-  white-space: nowrap;
-  font-weight: bold;
-  color: #fff;
-}
+  .post-date {
+    font-size: 0.95em;
+    white-space: nowrap;
+    font-weight: bold;
+    color: var(--text-color);
+  }
 
-.post-content {
-  font-size: 0.95em;
-  color: #e2d2e9;
-  white-space: pre-wrap;
-  margin-top: 4px;
-}
-
+  .post-content {
+    font-size: 0.95em;
+    color: #e2d2e9;
+    white-space: pre-wrap;
+    margin-top: 4px;
+  }
 </style>
