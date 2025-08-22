90335b9d84a35e143727cfbb6a1f467de7e163d4
Nikol <143750294+nar-ran@users.noreply.github.com>
2025-08-21 20:46:52 -0500

    Merge branch 'master' of https://github.com/nar-ran/me-just-me-blog

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/me-just-me-blog/',
+  // publicPath: '/me-just-me-blog/',
   chainWebpack: (config) => {
     config.plugin('html').tap((args) => {
       args[0].title = 'Me, Just Me';
